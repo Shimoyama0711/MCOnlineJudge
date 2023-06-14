5e16bf289cb17edf3d17163d27936f025475ca02
Shimoyama Ayumu <m19284@g.metro-cit.ac.jp>
2023-06-14 12:09:42 +0900

    提出部分追加

--- public/js/problem.js
+++ public/js/problem.js
@@ -9,4 +9,46 @@ $(function () {
         .catch((err) => {
             console.error(err);
         });
+
+    let editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
+        mode: "text/x-java",
+        lineNumbers: true,
+        theme: "default",
+        tabSize: 4
+    });
+
+    const uuid = getCookieFromKey("uuid");
+    const submitDiv = $("#submit-div");
+
+    if (uuid !== null) {
+        const versions = ["Java 8", "Java 20"];
+        const selectVersion = $("#select-version");
+
+        for (let i = 0; i < versions.length; i++) {
+            selectVersion.append($("<option>", {
+                value: i,
+                text: versions[i]
+            }));
+        }
+    } else {
+        submitDiv.css("display", "none");
+    }
 });
+
+function getCookieFromKey(key) {
+    const cookies = document.cookie;
+    const array = cookies.split(";");
+
+    let result = undefined;
+
+    for (let i = 0; i < array.length; i++) {
+        const content = array[i].split("=");
+
+        if (content[0] === key) {
+            result = content[1];
+            break;
+        }
+    }
+
+    return result;
+}
