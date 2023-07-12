cce9ff63cb5d2b4ae0eafe017ccd396c69fdf715
Shimoyama Ayumu <m19284@g.metro-cit.ac.jp>
2023-07-12 16:20:33 +0900

    サバ

--- servlet/MyServer.java
+++ servlet/MyServer.java
@@ -14,8 +14,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
